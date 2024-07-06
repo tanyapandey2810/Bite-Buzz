bcc16bd4e3d6095449c9f8d2015e07b2194e5493
Tanya Pandey <133050999+tanyapandey2810@users.noreply.github.com>
2024-07-07 02:34:20 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -6,7 +6,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 //config variables
 const currency = "usd";
 const deliveryCharge = 3;
-const frontend_URL = 'http://localhost:5173';
+const frontend_URL = 'https://bite-buzz-frontend.onrender.com';
 
 // Placing User Order for Frontend using stripe
 const placeOrder = async (req, res) => {
